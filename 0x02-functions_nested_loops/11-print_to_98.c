efecb6e2c6fbb58d6f37f8a8264134ae5d839702
ella-OA <owusuasamoahemmanuella@gmail.com>
2023-06-20 22:43:49 -0700

    c-functions

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,34 +1,44 @@
+#include "main.h"
 #include <stdio.h>
-	#include "main.h"
 
-
-	/**
-	 * print_to_98 - prints all natural numbers from n to 98,
-	 * followed by a new line
-	 * @n: print from this number
-	 */
-	void print_to_98(int n)
+/**
+  * print_to_98 - Prints all natural numbers from n to 98
+  * @n: The number to start printing from
+  *
+  * Return: Always 0.
+  */
+void print_to_98(int n)
+{
+	if (n <= 98)
 	{
-		int i, j;
-
-
-		if (n <= 98)
+		for (; n <= 98; n++)
 		{
-			for (i = n; i <= 98; i++)
+			if (n == 98)
 			{
-				if (i != 98)
-					printf("%d, ", i);
-				else if (i == 98)
-					printf("%d\n", i);
+				printf("%d", n);
+				printf("\n");
+				break;
 			}
-		} else if (n >= 98)
-		{
-			for (j = n; j >= 98; j--)
+			else
 			{
-				if (j != 98)
-					printf("%d, ", j);
-				else if (j == 98)
-					printf("%d\n", j);
+				printf("%d, ", n);
 			}
 		}
 	}
+	else
+	{
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+}
--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,43 @@
 #include "main.h"
+
 /**
- * times_table -prints tables
- *
- * Return: Always 0.
- */
+  * times_table - Prints the 9 times table, starting with 0
+  *
+  * Return: empty output
+  */
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
-	for (a = 0; a <= 9; a++)
+	int x, y, z, u, d;
+
+	for (x = 0; x <= 9; x++)
 	{
-		for (b = 0; b <= 9; b++)
+		for (y = 0; y <= 9; y++)
 		{
-			c = a * b;
-			if ((c / 10) == 0)
+			z = x * y;
+
+			if (z > 9)
 			{
-				if (b == 0)
-				{
-					_putchar ('0');
-				}
-				if (b != 0)
-				{
-					_putchar (' ');
-					_putchar ((c % 10) + '0');
-				}
-				if (b < 9)
-				{
-					_putchar(',');
-					_putchar (' ');
-				}
+				u = z % 10;
+				d = (z - u) / 10;
+
+				_putchar(44);
+				_putchar(32);
+				_putchar(d + '0');
+				_putchar(u + '0');
 			}
 			else
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b < 9)
+				if (y != 0)
 				{
-					_putchar(',');
-					_putchar (' ');
+					_putchar(44);
+					_putchar(32);
+					_putchar(32);
 				}
+
+				_putchar(z + '0');
 			}
 		}
-		_putchar ('\n');
+
+		_putchar('\n');
 	}
 }
